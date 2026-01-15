6613965b5bb0452502174dc2b2f55e832fdc2e20
Philippe Raemy <praemy61@gmail.com>
2026-01-15 10:00:00 +0100

    Draft stow

--- Phonetap/phonetap_cli.py
+++ Phonetap/phonetap_cli.py
@@ -26,9 +26,10 @@ def profile():
 def picture():
     """pictures operations"""
 
-# @cli.group()
-# def stow():
-#     """stow operations"""
+
+@cli.group()
+def stow():
+    """stow operations"""
 
 
 def profile_option(f):
@@ -121,15 +122,61 @@ def pictures_deduplicate(**kwargs):
     pictures.deduplicate(**kwargs)
 
 
-@cli.command('stow')
-@click.option('--folder', required=False, default=".", help='The source folder')
-@click.option('--target', required=False, default=".", help='The root target folder')
-@click.option('--dry-run', required=False, is_flag=True, help='Only show work at hand')
-@click.option('--verbose', required=False, is_flag=True, help='verbose progress display')
-@click.option('--time-offset', required=False, type=int, default=0, help='Set time offset in hours')
-@click.option('--suffix', required=False, type=str, default='', help='Set optional picture name suffix')
+def stow_options(f):
+    options = [
+        click.option('--source', '-s',required=False, default=".", help='The source folder'),
+        click.option('--target', '-t', required=False, default=".", help='The root target folder'),
+        click.option('--dry-run','-n',  required=False, is_flag=True, help='Only show work at hand'),
+        click.option('--force', '-f', required=False, is_flag=True, help='Force copy or move onto existing file'),
+        click.option('--verbose','-v',  required=False, is_flag=True, help='verbose progress display'),
+        click.option('--offset-hours', '-o', required=False, type=int, default=0, help='Set time offset in hours'),
+        click.option('--suffix', '-x', required=False, type=str, default='', help='Set optional picture name suffix'),
+        click.option('--filter', '-l', required=False, type=str, default='', help='Filter files with os-wildcards'),
+        click.option('--file-types','-y',  required=False, type=str, default='jpg,jpeg,mov,mp3,mp4,cr2,webp,avi,wav', help='File extensions of interest, comma-delimited, no wildcards'),
+        click.option('--min-date', '-max', required=False, type=str, default='1900-01-01', help='Minimum file date'),
+        click.option('--max-date', '-min', required=False, type=str, default='2500-01-01', help='Maximum file date'),
+
+    ]
+    return reduce(lambda lf, opt: opt(lf), options, f)
+
+
+@stow.command('copy')
+@stow_options
 def stow_pictures(**kwargs):
-    stowage.stow(**kwargs)
+    """
+This function imports pictures and other acceptable files from the
+current working folder (usually a memory card), or a designated source folder,
+and imports them in a local folder.\n
+On the way, the filenames are timestamped, i.e. the date and time of
+creation of the file are used to prefix the file name, in an ISO format.
+Additionally, the files are arranged under the chosen target folder
+in a directory tree similar to\n
+    ../yyyy/yyyymm/yyyymmdd\n
+If such a directory does not exist, it is created.
+If such a directory does exist, or a similar directory with a name or
+a description suffix after the day date, this directory is used.
+    """
+    stowage.stow('copy', **kwargs)
+
+
+@stow.command('move')
+@stow_options
+def stow_pictures(**kwargs):
+    """
+This function imports pictures and other acceptable files from the
+current working folder (usually a memory card), or a designated source folder,
+and imports them in a local folder.\n
+With this command the imported files are deleted from the source location.\n
+On the way, the filenames are timestamped, i.e. the date and time of
+creation of the file are used to prefix the file name, in an ISO format.
+Additionally, the files are arranged under the chosen target folder
+in a directory tree similar to\n
+    ../yyyy/yyyymm/yyyymmdd\n
+If such a directory does not exist, it is created.
+If such a directory does exist, or a similar directory with a name or
+a description suffix after the day date, this directory is used.
+    """
+    stowage.stow('move', **kwargs)
 
 
 if __name__ == '__main__':
--- Phonetap/stowage.py
+++ Phonetap/stowage.py
@@ -1,2 +1,76 @@
-def stow(folder: str, target: str, dry_run: bool, verbose: bool, time_offset: int, suffix: str):
-    return None
+import os
+import shutil
+from datetime import datetime, timedelta
+import re
+
+
+def stow(command: str, source: str, target: str, dry_run: bool, force: bool, verbose: bool, offset_hours: int,
+         suffix: str, filter: str, file_types: str, min_date: str, max_date: str):
+    extensions = ['.' + e for e in file_types.split(',')]
+    if filter:
+        filter_re = re.compile(filter.replace('.', '\\.').replace('?', '.').replace('*', '.*'), re.IGNORECASE)
+    re_timestamp = re.compile(
+        r'^(?P<head>.*)(?P<yyyy>\d\d\d\d)-(?P<MM>\d\d)-(?P<dd>\d\d)_(?P<hh>\d\d)-(?P<mm>\d\d)(-(?P<ss>\d\d))?(?P<tail>.*)\.(?P<ext>.*)$')
+    min_date = datetime.fromisoformat(min_date)
+    max_date = datetime.fromisoformat(max_date)
+    target_folders = {}
+
+    def find_target(creation_day: datetime):
+        creation_day = creation_day.date()
+        if creation_day in target_folders:
+            return target_folders[creation_day]
+
+        target_root = os.path.join(target,
+                                   creation_day.strftime("%Y"),
+                                   creation_day.strftime("%Y%m"))
+        leaf = creation_day.strftime("%Y%m%d")
+        found_leaf = next(
+            (d
+             for _, dirs, _ in os.walk(target_root)
+             for d in dirs
+             if d == leaf or d.startswith(leaf + ' ')
+             ), leaf
+        )
+        target_folder = os.path.join(target_root, found_leaf)
+        target_folders[creation_day] = target_folder
+        if not os.path.exists(target_folder):
+            os.makedirs(target_folder, exist_ok=True)
+            return target_folder
+
+        for folder, _, files in os.walk(source):
+            for file in files:
+                if not any((file.endswith(e) for e in extensions)):
+                    continue
+                if filter and not filter_re.match(file):
+                    continue
+                path_to_file = os.path.join(folder, file)
+                ma = re_timestamp.match(file)
+                if ma:  # no need to rename
+                    ma_di = ma.groupdict()
+                    creation_time = datetime(
+                        int(ma_di['yyyy']), int(ma_di['MM']), int(ma_di['dd']),
+                        int(ma_di['hh']), int(ma_di['mm']), int(ma_di['ss']) if ma_di['ss'] else 0)
+                    head = ma_di['head'].strip()
+                    tail = ma_di['tail'].strip()
+                else:
+                    creation_time = os.path.getctime(path_to_file)
+                    head = ''
+                    chunks = file.strip().split('.')
+                    tail = '.'.join(chunks[:-1])
+                    ext = chunks[-1]
+                if offset_hours:
+                    creation_time += timedelta(hours=offset_hours)
+                if not min_date <= creation_time <= max_date:
+                    continue
+                target_folder = find_target(creation_time)
+                new_file = os.path.join(target_folder,
+                                        f"{creation_time.strftime("%Y-%m-%d_%H_%m_%S")} {head}{tail}{suffix}.{ext}")
+                if os.path.exists(new_file):
+                    if force:
+                        os.remove(new_file)
+                    else:
+                        continue
+                if command == 'copy':
+                    shutil.copy(path_to_file, new_file)
+                else:
+                    shutil.move(path_to_file, new_file)
